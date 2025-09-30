21ae8f76ab50c832b190b9d5d29f7968b458bc68
mavcr <mianvacrro@gmail.com>
2025-09-30 06:48:29 +0000

    name appointment

--- backend/src/main/java/com/plexsalud/plexsalud/appointment/infrastructure/repositories/AppointmentRepository.java
+++ backend/src/main/java/com/plexsalud/plexsalud/appointment/infrastructure/repositories/AppointmentRepository.java
@@ -14,9 +14,9 @@ import org.springframework.data.repository.CrudRepository;
 
 @Repository
 public interface AppointmentRepository extends CrudRepository<Appointment, Long> {
-    @Query("SELECT a.id, a.date, a.status FROM Appointment a WHERE a.doctor = :doctor")
+    @Query("SELECT a.id, a.date, a.status, p.fullName FROM Appointment a Join a.patient p WHERE a.doctor = :doctor")
     List<AppointmentResponse> findAllAppointmentsByDoctor(Doctor doctor);
 
-    @Query("SELECT a.id, a.date, a.status FROM Appointment a WHERE a.patient = :patient")
+    @Query("SELECT a.id, a.date, a.status, d.fullName FROM Appointment a JOIN a.doctor d WHERE a.patient = :patient")
     List<AppointmentResponse> findAllAppointmentsByPatient(Patient patient);
 }
